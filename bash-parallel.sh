2639accba4b835d202f7748f10b8a2812cafa44b
Bryan Klumpp <bryan-klumpp-not-my-real-email.com>
2025-02-01 14:20:09 -0600

    bash-parallel.sh - switched to use subshells

--- bash-parallel.sh
+++ bash-parallel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Please replace this entire file with a bash script that takes as piped input multiple lines of bash code.  It will execute these in parallel in the current shell instance, using eval so that it is able to share in-scope environment variables.  It will accept as optional arguments -j or --max-procs (like gnu parallel) to limit how many statements can run in parallel at once; if neither -j nor --max-procs is specified, the default will be equal to the number of real processor cores (if you can limit it to the number of P-cores or performance cores that would be even better).  Each line's output will be logged to a file dedicated to that line, and not to the console; stdout and stderr will be mixed, writing to the same file; the name of the log file will be patterned as <derived_basename>_running.log where derived_basename is the first 50 characters of the output, with any characters that are not legal in both ext4 and ntfs filename replaced with an underscore, and all spaces also replaced with an underscore.  These log files will be placed in a subdirectory called execlog; this subdirectory will be created if it does not exist.  Upon each line completing execution, the log file name will be renamed to <derived_basename>_exitcode<exitcode>.log where <exitcode> is the exit code of the line's execution.  The main script will block until all lines are complete and will print a status, updated once a second, of how many lines are not yet completed.  After all lines have exited, it will print a list of the log file paths (fully-qualified file names) for all logs that do not end in "exitcode0.log" It should be executable on Ubuntu bash or WSL2.  Include all of this text I've entered in this prompt (with this sentence also) as a single-line comment at the top of the file for documentation and easy updates.
+# Please provide a single source script file with a bash script that takes as piped input multiple lines of bash code; the script will execute these lines in parallel as subshells.  Lines can contain multiple statements separated by a semicolon, but each line should be execute as a whole in the subshell.  The script will accept as optional arguments -j or --max-procs (like gnu parallel) to limit how many statements can run in parallel at once; if neither -j nor --max-procs is specified, the default will be equal to the number of real processor cores (if you can limit it to the number of P-cores or performance cores that would be even better).  Each line's output will be logged to a file dedicated to that line, and not to the console; stdout and stderr will be mixed, writing to the same file; the name of the log file will be patterned as <derived_basename>_running.log where derived_basename is the first 50 characters of the output, with any characters that are not legal in both ext4 and ntfs filename replaced with an underscore, and all spaces also replaced with an underscore.  These log files will be placed in a subdirectory called execlog; this subdirectory will be created if it does not exist.  Upon each line completing execution, the log file name will be renamed to <derived_basename>_exitcode<exitcode>.log where <exitcode> is the exit code of the line's subshell execution.  The main script will block until all lines are complete and will print a status, updated once a second, printing the lines that are not yet completed.  Each status update will replace the previous status update so that there is only a single status update visible on the screen.   After all lines have exited, it will print a list of the log file paths (fully-qualified file names) for all logs that do not end in "exitcode0.log" It should be executable on Ubuntu bash or WSL2.  Before execution, this script will delete any log files from previous runs.  Include all of this text I've entered in this prompt (with this sentence also) as a single-line comment at the top of the file for documentation and easy updates.
 
-# Create execlog directory if it doesn't exist
+# Create execlog directory if it doesn't exist and clear previous logs
 mkdir -p execlog
+rm -f execlog/*
 
 # Default number of parallel jobs to the number of processor cores
 DEFAULT_JOBS=$(nproc --all)
@@ -38,7 +39,7 @@ for (( i=0; i<${#LINES[@]}; i++ )); do
     LINE="${LINES[$i]}"
     BASENAME=$(sanitize_filename "$LINE")
     LOGFILE="execlog/${BASENAME}_running.log"
-    eval "$LINE" &> "$LOGFILE"
+    eval "{ $LINE; }" &> "$LOGFILE"
     EXITCODE=$?
     mv "$LOGFILE" "execlog/${BASENAME}_exitcode${EXITCODE}.log"
   } &
@@ -52,10 +53,10 @@ while (( ${#PIDS[@]} )); do
       PIDS=(${PIDS[@]/$PID/})
     fi
   done
-  echo "Jobs remaining: ${#PIDS[@]}"
+  echo -ne "Jobs remaining: ${#PIDS[@]}\r"
   sleep 1
 done
 
 # Print list of log file paths for logs that do not end in "exitcode0.log"
-echo "Execution complete. Log files with non-zero exit codes:"
+echo -e "\nExecution complete. Log files with non-zero exit codes:"
 find "$(pwd)/execlog" -type f ! -name '*exitcode0.log'
